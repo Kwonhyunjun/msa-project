d1c7ca35a16da74d16c012c30fe9714e2ecd652e
권현준 <bonheur9813@naver.com>
2024-12-01 14:48:49 +0900

    init : product 프로젝트 생성 #1

---
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/ProductController.java
@@ -0,0 +1,19 @@
+package com.sparta.msa_exam.product;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/products")
+
+public class ProductController {
+
+    @GetMapping("")
+    public String test(){
+        return "OK";
+    }
+
+}
