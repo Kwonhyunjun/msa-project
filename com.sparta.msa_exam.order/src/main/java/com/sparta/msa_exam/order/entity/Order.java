7184517c7008939a3042b12ba71adf5cc2b5b118
권현준 <bonheur9813@naver.com>
2024-12-03 00:44:57 +0900

    feat : Order 엔티티 생성 #3

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/entity/Order.java
@@ -0,0 +1,26 @@
+package com.sparta.msa_exam.order.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderProduct> product_ids = new ArrayList<>();
+    
+}
