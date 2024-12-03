6231305555eecc80c8cef3edce6693c583d92485
권현준 <bonheur9813@naver.com>
2024-12-04 03:48:19 +0900

    feat : Auth, Order 응답헤더 서버 포트 추가

---
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/filter/ServerPortHeaderFilter.java
@@ -0,0 +1,23 @@
+package com.sparta.msa_exam.auth.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ServerPortHeaderFilter implements Filter {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        httpResponse.setHeader("Server-Port", serverPort);
+        chain.doFilter(request, response);
+    }
+}
